d2098ac16cdd1f50d55b59f6065d20ff756ccab0
CeddlyBurge <CeddlyBurge@gmail.com>
2017-03-28 17:38:23 +0100

    adding comments and renames. also move the paragrapher parser before apply the other formatters

--- Markdown.cs
+++ Markdown.cs
@@ -6,6 +6,8 @@ using System.Text.RegularExpressions;
 // would like to use contracts and use static analysis to check for problems but I think .net core doesn't support it at the moment
 //using System.Diagnostics.Contracts;
 
+
+// there are a variety of interfaces and classes in this file, which should all be in their own files, but I don't think exercism supports this.
 public class MarkdownInternalException : Exception
 {
     public MarkdownInternalException(string message) : base(message)
@@ -72,6 +74,7 @@ internal class MarkdownToHtmlTagBase
 
     static string Markdown_IndicatorsReplacedWithHtmlEmTags(string markdown) => SingleMarkdownIndicatorsReplacedWithHtmlTags(markdown, "_", "em");
 
+    // this name isn't the most amazing, but couldn't think of anything better
     static string SingleMarkdownIndicatorsReplacedWithHtmlTags(string markdown, string markdownIndicator, string htmlTag)
     {
         var pattern = markdownIndicator + "(.+)" + markdownIndicator;
@@ -159,12 +162,11 @@ public class Markdown
     {
         Initialise(markdown);
 
-        MoveToFirstLine();
-
         while (CurrentLineExists)
         {
             int currentLineIndex = CurrentLineIndex;
-            ParseCurrentLine();
+
+            ParseAtCurrentLine();
 
             if (currentLineIndex == CurrentLineIndex)
                 throw new MarkdownInternalException($"Internal parser error. Line '{CurrentLine}' would have caused an infinite loop.");
@@ -173,13 +175,21 @@ public class Markdown
         return Html;
     }
 
-    void ParseCurrentLine()
+    void Initialise(string markdown)
     {
-        markdownToHtmls.ToList().ForEach(m => ApplyMarkdownToHtml(m));
+        ioCoordinator.Initialise(markdown.Split('\n').ToList());
 
+        MoveToFirstLine();
+    }
+
+    void ParseAtCurrentLine()
+    {
         // the paragraph parser is special, in that it applies if nothing else is able to work
-        if (markdownToHtmls.Any(m => m.CanParseCurrentLine) == false && CurrentLineExists)
+        if (CurrentLineExists && markdownToHtmls.Any(m => m.CanParseCurrentLine) == false)
             paragraph.WriteParagraphTag();
+
+        // this could apply more than one markdownToHtml, as all the parsers are called and they update CurrentLine as they go.
+        markdownToHtmls.ToList().ForEach(m => ApplyMarkdownToHtml(m));
     }
 
     void ApplyMarkdownToHtml(IMarkdownToHtmlTag markdownToHtml)
@@ -187,8 +197,6 @@ public class Markdown
         if (markdownToHtml.CanParseCurrentLine)
             markdownToHtml.WriteHtmlTag();
     }
-
-    void Initialise(string markdown) => ioCoordinator.Initialise(markdown.Split('\n').ToList());
 
     void MoveToFirstLine() => ioCoordinator.MoveToFirstLine();
     int CurrentLineIndex => ioCoordinator.CurrentLineIndex;
