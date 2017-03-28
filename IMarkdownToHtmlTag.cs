9f868c9503bfa7d0f6b1e24eb3a2ed1a7c575086
CeddlyBurge <CeddlyBurge@gmail.com>
2017-03-28 18:28:27 +0100

    remove redundant comments

--- IMarkdownToHtmlTag.cs
+++ IMarkdownToHtmlTag.cs
@@ -1,5 +1,4 @@
-﻿// there are a variety of interfaces and classes in this file, which should all be in their own files, but I don't think exercism supports this.
-namespace MarkdownToHtml
+﻿namespace MarkdownToHtml
 {
     internal interface IMarkdownToHtmlTag
     {
--- MarkdownInternalException.cs
+++ MarkdownInternalException.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 
-// there are a variety of interfaces and classes in this file, which should all be in their own files, but I don't think exercism supports this.
 namespace MarkdownToHtml
 {
     public class MarkdownInternalException : Exception
